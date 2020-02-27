c5d3477e4397cc7346b5dcabb401aa25f378facc
Alexander Smirnov <alex.f.smirnov@gmail.com>
2020-02-27 12:13:03 +0100

    initial steps on character stats download

--- src/helpers/dnd-beyond/getCharacterStats.ts
+++ src/helpers/dnd-beyond/getCharacterStats.ts
@@ -4,12 +4,21 @@ export interface GetCharacterStatsProps {
     characterUrl: string;
 }
 
+export interface CharacterStats {
+    currentHp: number;
+    maxHp: number;
+    tempHp: number;
+    armorClass: number;
+    passivePerception: number;
+}
+
 const getCharacterStats = async ({ characterUrl }: GetCharacterStatsProps) => {
-    const result = await axios.get(characterUrl);
+    const response = await axios({ method: 'GET', url: characterUrl });
+    const html = response.data;
+    const doc = new DOMParser().parseFromString(html, 'text/html');
+    console.log(doc);
 
-    console.log(result);
-
-    return result;
+    return html;
 };
 
 export default getCharacterStats;
