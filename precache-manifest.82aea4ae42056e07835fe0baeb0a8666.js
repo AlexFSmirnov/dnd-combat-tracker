f5924142136d9e33744328721c6675bf8f31ce7f
Alexander Smirnov <alex.f.smirnov@gmail.com>
2020-03-02 18:28:09 +0100

    Updates

---
+++ precache-manifest.82aea4ae42056e07835fe0baeb0a8666.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea81422b5ca67b8aeb43d0e4b7a09e61",
+    "url": "/dnd-combat-tracker/index.html"
+  },
+  {
+    "revision": "a7cc7c674f3fc701878f",
+    "url": "/dnd-combat-tracker/static/css/main.8edfccb4.chunk.css"
+  },
+  {
+    "revision": "9cc7a9254909471bb7f9",
+    "url": "/dnd-combat-tracker/static/js/2.d5989e58.chunk.js"
+  },
+  {
+    "revision": "20299834ee5cd8322f54b4cd7cafff31",
+    "url": "/dnd-combat-tracker/static/js/2.d5989e58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7cc7c674f3fc701878f",
+    "url": "/dnd-combat-tracker/static/js/main.ee83acce.chunk.js"
+  },
+  {
+    "revision": "4c70f37a4836c3bdf7db",
+    "url": "/dnd-combat-tracker/static/js/runtime-main.4f8f8b08.js"
+  }
+]);
