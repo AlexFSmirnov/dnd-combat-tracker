8367cdb9bc8801e00561f3e5b3f10f0ed731617f
Alexander Smirnov <alex.f.smirnov@gmail.com>
2020-03-03 10:20:59 +0100

    reverted to non-experimental react

--- src/index.tsx
+++ src/index.tsx
@@ -4,5 +4,6 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />);
+// ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />);
+ReactDOM.render(<App />, document.getElementById('root'));
 serviceWorker.unregister();
