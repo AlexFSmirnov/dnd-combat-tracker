33360fed1426dd3ab53b0ad7e1884e97839b6f0c
Alexander Smirnov <alex.f.smirnov@gmail.com>
2020-03-27 16:16:12 +0100

    Updates

--- precache-manifest.795c99d101cf1c60e1c7f6d980bf1020.js
+++ precache-manifest.795c99d101cf1c60e1c7f6d980bf1020.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a98ac565c21d2897ef285ec88218566e",
+    "revision": "0cc75697b069dbab836fe18dd3e63642",
     "url": "/dnd-combat-tracker/index.html"
   },
   {
-    "revision": "bf5c8637475d67c50ba2",
+    "revision": "1c35d8a6f745e4db2d1a",
     "url": "/dnd-combat-tracker/static/css/main.029029bc.chunk.css"
   },
   {
-    "revision": "45298fe6f6c47b43e0c3",
-    "url": "/dnd-combat-tracker/static/js/2.d19a8932.chunk.js"
+    "revision": "9e3ad6692839d80e8d3f",
+    "url": "/dnd-combat-tracker/static/js/2.5e10c879.chunk.js"
   },
   {
     "revision": "17b38651a42342e955094dba09a63fe1",
-    "url": "/dnd-combat-tracker/static/js/2.d19a8932.chunk.js.LICENSE.txt"
+    "url": "/dnd-combat-tracker/static/js/2.5e10c879.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "bf5c8637475d67c50ba2",
-    "url": "/dnd-combat-tracker/static/js/main.38f9d1d0.chunk.js"
+    "revision": "1c35d8a6f745e4db2d1a",
+    "url": "/dnd-combat-tracker/static/js/main.238592df.chunk.js"
   },
   {
     "revision": "4c70f37a4836c3bdf7db",
