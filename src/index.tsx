dd8afb2b4420ba65ee3d5de9367661109d829c9b
Alexander Smirnov <alex.f.smirnov@gmail.com>
2020-03-28 17:45:59 +0100

    enabled service workers

--- src/index.tsx
+++ src/index.tsx
@@ -6,4 +6,4 @@ import * as serviceWorker from './serviceWorker';
 
 // ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />);
 ReactDOM.render(<App />, document.getElementById('root'));
-serviceWorker.unregister();
+serviceWorker.register();
